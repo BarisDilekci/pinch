ba0b1c7a023c967008fadc8ad38f15fb4d607e40
BarisDilekci <bdilekci200@gmail.com>
2024-07-06 02:35:32 +0300

    feat: drag gesture operation done

--- pinch/View/ContentView.swift
+++ pinch/View/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     //MARK: - PROPERTIES
     @State private var isAnimating : Bool = true
     @State private var imageScale : CGFloat = 1
+    @State private var imageOffset : CGSize = .zero
     
     
     //MARK: - LIFE CYCLE
@@ -27,6 +28,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .animation(.linear(duration: 1), value: isAnimating)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 
                 //MARK: - TAP GESTURE
@@ -41,6 +43,23 @@ struct ContentView: View {
                             }
                         }
                     }
+                //MARK: - DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    withAnimation(.spring()) {
+                                        imageScale = 1
+                                        imageOffset = .zero
+                                    }
+                                }
+                            })
+                    )
                 
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
