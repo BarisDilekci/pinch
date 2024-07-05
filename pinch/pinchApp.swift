567d6cf3976466c57719f7c0c91b744309028290
BarisDilekci <bdilekci200@gmail.com>
2024-07-05 22:06:59 +0300

    Initial Commit

---
+++ pinch/pinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinchApp.swift
+//  pinch
+//
+//  Created by Barış Dilekçi on 5.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct pinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
